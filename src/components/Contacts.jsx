79b54839bd3569fadd96d5b6c90baa8286caa4fe
AniruddhUdayan <aniruddhudayan34@gmail.com>
2024-02-23 22:46:12 +0530

    fix

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,10 +2,7 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
-
-// import { styles } from "../styles";
 import  EarthCanvas  from "@/components/Earth";
-// import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
 const Contacts = () => {
